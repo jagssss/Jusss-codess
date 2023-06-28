70cf4947f5ee759fefd11660eca4691123373728
jagss <133646701+jagssss@users.noreply.github.com>
2023-06-28 19:46:28 +0530

    Time: 68 ms (23.64%), Space: 42.1 MB (28.16%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        const fns = fn(...args);
+
+        const p = new Promise((res, rej) => {
+            setTimeout(() => {
+                rej('Time Limit Exceeded')
+            }, t);
+        })
+
+        return Promise.race([fns, p]);
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
